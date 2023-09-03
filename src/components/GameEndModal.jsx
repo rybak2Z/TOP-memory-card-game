01ed66a1eef8df791d9edf8f5c8f06fd20ebb9ac
rybak2Z <rybak2Z.000@gmail.com>
2023-09-03 11:56:10 +0200

    Style modal window

--- src/components/GameEndModal.jsx
+++ src/components/GameEndModal.jsx
@@ -4,11 +4,19 @@ function GameEndModal({ won, score, maxScore, onReset, onGoToMainMenu }) {
   const gameStatusText = won ? 'You won!' : 'Game over';
 
   return (
-    <div className='modal'>
-      Score: {score} / {maxScore}
-      {gameStatusText}
-      {!won && <button onClick={onReset}>Restart</button>}
-      <button onClick={onGoToMainMenu}>Main Menu</button>
+    <div className='modal-wrapper'>
+      <div className='modal'>
+        <h1>{gameStatusText}</h1>
+        {!won && (
+          <h2>
+            Score: {score} / {maxScore}
+          </h2>
+        )}
+        <div className='buttons'>
+          {!won && <button onClick={onReset}>Restart</button>}
+          <button onClick={onGoToMainMenu}>Main Menu</button>
+        </div>
+      </div>
     </div>
   );
 }
