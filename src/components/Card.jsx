0e09bc7c6b48d3838c7d94597433c8174d404bd5
rybak2Z <rybak2Z.000@gmail.com>
2023-09-03 17:00:57 +0200

    Simplify Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,17 +2,15 @@ import '../styles/Card.css';
 import placeHolderImage from '../images/placeholder.webp';
 
 function Card({ cardIdx, label, hide, onClick }) {
-  const activeClass = hide ? 'active' : '';
+  const flipClass = hide ? 'flip' : '';
   return (
-    <div className={'card ' + activeClass}>
-      <button onClick={() => onClick(cardIdx)}>
-        <img src={placeHolderImage} />
-        <div className='card-label'>
-          <h1>{label}</h1>
-          <h2>Location</h2>
-        </div>
-      </button>
-    </div>
+    <button onClick={() => onClick(cardIdx)} className={'card ' + flipClass}>
+      <img src={placeHolderImage} />
+      <div className='card-label'>
+        <h1>{label}</h1>
+        <h2>Location</h2>
+      </div>
+    </button>
   );
 }
 
